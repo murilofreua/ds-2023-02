7af32b29407ed8fb0360a59eb920a6c1ca5ea86a
Murilo Freua <106319500+murilofreua@users.noreply.github.com>
2023-10-02 21:47:47 -0300

    Create MetodoDeNewton.java

---
+++ newton/MetodoDeNewton.java
@@ -0,0 +1,29 @@
+public class MetodoDeNewton {
+    
+  static double calculaRaizQuadrada(double inteiro, double tolerancia) {
+        // Variável auxiliar
+        double x = inteiro;
+
+        // Raíz a ser calculada
+        double raiz;
+
+        // Número de iterações
+        int count = 0;
+
+        while (true) {
+            count++;
+
+            // Calcula o número mais próximo
+            raiz = 0.5 * (x + (inteiro / x));
+
+            // Checa a proximidade calculada
+            if (Math.abs(raiz - x) < tolerancia)
+                break;
+
+            // Atualiza a raíz
+            x = raiz;
+        }
+
+        return raiz;
+    }
+}
